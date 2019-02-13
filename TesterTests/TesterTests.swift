4c8ca29f86c4d136a069bab92b7a80092e647246
Mateusz Zając <cojoj@icloud.com>
2019-02-13 10:18:14 +0100

    Added test

--- TesterTests/TesterTests.swift
+++ TesterTests/TesterTests.swift
@@ -20,8 +20,7 @@ class TesterTests: XCTestCase {
     }
 
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(true)
     }
 
     func testPerformanceExample() {
